82074348cc4082483a11cf71e47bbe55064b2edf
mohalisalem <ali.salem.engineer@gmail.com>
2024-08-05 22:47:29 +0200

    feat: add exception classes

--- src/main/java/tn/association/management/web/exception/EntityAlreadyExistsException.java
+++ src/main/java/tn/association/management/web/exception/EntityAlreadyExistsException.java
@@ -1,4 +1,17 @@
 package tn.association.management.web.exception;
 
-public class EntityAlreadyExistsException {
+public class EntityAlreadyExistsException extends RuntimeException {
+
+    public final String message;
+
+    public EntityAlreadyExistsException(String message, Throwable cause) {
+        super(message, cause);
+        this.message = message;
+    }
+
+    public EntityAlreadyExistsException(String message) {
+        super(message);
+        this.message = message;
+    }
+
 }
--- src/main/java/tn/association/management/web/exception/EntityNotFoundException.java
+++ src/main/java/tn/association/management/web/exception/EntityNotFoundException.java
@@ -1,4 +1,17 @@
 package tn.association.management.web.exception;
 
-public class EntityNotFoundException {
+public class EntityNotFoundException extends RuntimeException {
+
+    public final String message;
+
+    public EntityNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+        this.message = message;
+    }
+
+    public EntityNotFoundException(String message) {
+        super(message);
+        this.message = message;
+    }
+
 }
